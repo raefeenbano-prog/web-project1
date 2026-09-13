06a825483415e1f7837ef0933ea81dde352e58e2
raefeen bano <raefeenbano@gmail.com>
2026-09-13 13:39:07 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,90 +1,17 @@
-let gameSeq = [];
-let userSeq = [];
-
-let btns = ["red", "yellow", "green", "purple"];
-
-let started = false;
-let level = 0;
-
-let h2 = document.querySelector("h2");
-
-document.addEventListener("keypress", function () {
-    if (!started) {
-        console.log("Game Started");
-        started = true;
-        levelUp();
+const max = prompt("Enter the max number")
+const random = Math.floor(Math.random() * max) + 1;
+let guess = prompt("guess the number");
+while (true) {
+    if (guess == "quit") {
+        console.log("user quit");
+        break;
     }
-});
-
-function gameFlash(btn) {
-    btn.classList.add("flash");
-    setTimeout(() => {
-        btn.classList.remove("flash");
-    }, 250);
-}
-
-function userFlash(btn) {
-    btn.classList.add("userFlash");
-    setTimeout(() => {
-        btn.classList.remove("userFlash");
-    }, 250);
-}
-
-function levelUp() {
-    userSeq = [];
-    level++;
-    h2.innerText = `Level ${level}`;
-
-    let randIdx = Math.floor(Math.random() * 4);
-    let randColor = btns[randIdx];
-    gameSeq.push(randColor);
-
-    console.log(gameSeq);
-
-    let randBtn = document.querySelector(`.${randColor}`);
-    gameFlash(randBtn);
-}
-
-function checkAns(idx) {
-    if (userSeq[idx] === gameSeq[idx]) {
-
-        if (userSeq.length === gameSeq.length) {
-            setTimeout(levelUp, 1000);
-        }
-
+    if (guess == random) {
+        console.log("you are right.congrats!! your number was ", random);
+        break;
+    } else if (guess < random) {
+        guess = prompt("Hint:your number was too small,please try again");
     } else {
-        h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key to restart.`;
-
-        document.querySelector("body").style.backgroundColor = "red";
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor = "white";
-        }, 200);
-
-        reset();
+        guess = prompt("Hint:your number was too big,please try again");
     }
-}
-
-function btnPress() {
-    let btn = this;
-    userFlash(btn);
-
-    let userColor = btn.getAttribute("id");
-    userSeq.push(userColor);
-
-    console.log(userSeq);
-
-    checkAns(userSeq.length - 1);
-}
-
-let allBtns = document.querySelectorAll(".btn");
-
-for (let btn of allBtns) {
-    btn.addEventListener("click", btnPress);
-}
-
-function reset() {
-    started = false;
-    gameSeq = [];
-    userSeq = [];
-    level = 0;
 }
